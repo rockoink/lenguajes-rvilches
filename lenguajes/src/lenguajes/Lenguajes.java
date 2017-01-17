b6f3c3a895aa4d7fac237ef8d6b12f7e6001b749
rockoink <xrockox@techgeek.co.in>
2017-01-16 19:37:14 -0600

    Update Lenguajes.java

--- lenguajes/src/lenguajes/Lenguajes.java
+++ lenguajes/src/lenguajes/Lenguajes.java
@@ -17,6 +17,7 @@ public class Lenguajes {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo");
+        System.out.println("Hola mundo desde mi github");
         
     }
     
