3d4e5bb4ddfab8cfc46151e6f8733deafc262b15
T-102 <T-102@PC204>
2017-02-23 19:39:14 -0600

    contenido archivo sin terminar

--- lenguajes/src/excepciones2/ValidacionArchivo.java
+++ lenguajes/src/excepciones2/ValidacionArchivo.java
@@ -12,15 +12,37 @@ package excepciones2;
 import java.io.*;
 public class ValidacionArchivo {
     
-   public static void  validarExtencion(File file)throws ExtencionNoValidaException{
+   public static void  validarExtencion(File file)throws ExtencionNoValidaExeption{
        
       String nombre=file.getName();
       
      int indice1= nombre.indexOf(".");
      int indice2= nombre.length();
      String extencion=nombre.substring(indice1-1, indice2);
+    
      
 
-if(!extencion.equals("txt"))    throw new ExtencionNoValidaException();
+if(!extencion.equals("txt"))    throw new ExtencionNoValidaExeption();
+throw new ExtencionNoValidaExeption();
+
+
+   }
+   
+   public static void validarMayusculas(File file) throws MayusculasException{
+       
+       String nombre=file.getName();
+       int indice1=nombre.indexOf(".");
+       String name=nombre.substring(0,indice1);
+       
+       String mayusculas=name.toUpperCase();
+       
+       if(name.equals(mayusculas))throw new MayusculasException();
+       
+       
+   }
+   
+   public static void validarContenido(File file) throws ContenidoException{
+       
+       
    }
 }
