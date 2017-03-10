632f8501549dab92aec2912fa2dfc23792af19a7
T-102 <T-102@PC191>
2017-03-09 19:37:51 -0600

    serializacion

---
+++ lenguajes/src/serializar/PersistenciaUsuario.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serializar;
+import java.io.*;
+
+public class PersistenciaUsuario {
+    
+    /*
+    primer metodo de tipo accion guardar
+    
+    */
+    
+    public void guardar(Usuario u) throws Exception{
+        File file=new File("basesita-usuarios");
+        FileOutputStream fos=new FileOutputStream(file);
+        ObjectOutputStream oos=new ObjectOutputStream(fos);
+        oos.writeObject(u);
+        
+        
+    }
+}
