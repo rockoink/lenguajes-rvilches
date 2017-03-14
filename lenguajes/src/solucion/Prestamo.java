b5a857de898ab52657c81b7b590e8ce0f7eb120e
Sony <Sony@VAIO>
2017-03-13 23:57:28 -0600

    intento 1 problema

---
+++ lenguajes/src/solucion/Prestamo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solucion;
+
+/**
+ *
+ * @author Sony
+ */
+
+//excepcion solucion examen fsefwe
+public class Prestamo {
+    
+    public static void prestarAlumno(Alumno alumno) throws Exception{
+        if(alumno.getLibros().length>3)throw new Exception();
+        
+    }
+    
+    public static void prestarProfesor(Profesor profesor) throws Exception{
+        if(profesor.getLibros().length>=2)throw new Exception();
+        
+    }
+    
+}
