ec86da0fd3357740d448ab526a7662a4a74e0728
Sony <Sony@VAIO>
2017-03-13 23:55:14 -0600

    intento 1 problema

---
+++ lenguajes/src/serializar/Usuario.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serializar;
+
+/**
+ *
+ * @author Sony
+ */
+
+//clase usuario inicializar variables
+//dsvdsfes
+public class Usuario {
+    
+        private String nombre;
+    private String email;
+    private int edad;
+    
+    public Usuario(){
+        
+    }
+
+    public Usuario(String nombre,String email,int edad){
+        this.edad=edad;
+        this.email=email;
+        this.nombre=nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    
+}
